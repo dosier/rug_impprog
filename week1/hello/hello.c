a7e4258ccf81feb9ad7e8fd5bfe8df57c68f050f
Stan van der Bend <stanb3nd@gmail.com>
2018-09-05 01:27:56 +0200

    Hello world exercise, imperative programming week 1

---
+++ week1/hello/hello.c
@@ -0,0 +1,14 @@
+/* file: hello.c                                             */
+/* author: Stan van der Bend (email: stanvdbend@gmail.com)   */
+/* date: 5-9-2018                                            */
+/* version: 1.0                                              */
+/* Description: This program prints ’Hello world’            */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello world!\n");
+    return 0;
+}
