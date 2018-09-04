f6c968239ee0514eebfa82e38f55a32358a6e438
Stan van der Bend <stanb3nd@gmail.com>
2018-09-05 01:28:20 +0200

    Champagne exercise, imperative programming week 1

---
+++ week1/hello/champagne.c
@@ -0,0 +1,35 @@
+/* file: champagne.c                                         */
+/* author: Stan van der Bend (email: stanvdbend@gmail.com)   */
+/* date: 5-9-2018                                            */
+/* version: 1.0                                              */
+/* Description: This program prints ’Hello world’            */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(int argc, char *argv[]){
+
+    int peopleCount;
+
+    // read number of invited friends from the input (keyboard)
+    scanf("%d", &peopleCount);
+
+    // count user
+    peopleCount += 1;
+
+    // calculate total amount of champagne drank
+    int totalChampagneInML = peopleCount * 150;
+    // calculate the amount of bottles that need to be bought
+    int requiredChampagneBottles = totalChampagneInML / 1500;
+
+    // is there any leftover champagne?
+    if(totalChampagneInML % 1500 > 0){
+        requiredChampagneBottles++;
+    }
+
+    // print minimal amount of required champagne bottles
+    printf("%d", requiredChampagneBottles);
+
+    return 0;
+}
