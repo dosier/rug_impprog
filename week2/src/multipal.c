e5bed7bbf71b3ef44d80353c8f90b1187a9f50ee
Stan van der Bend <stanb3nd@gmail.com>
2018-09-17 01:39:16 +0200

    week 2 multipal done, triplets not done

--- week2/src/multipal.c
+++ week2/src/multipal.c
@@ -2,19 +2,42 @@
 /* author: Stan van der Bend (email: stanvdbend@gmail.com)   */
 /* date: 5-9-2018                                            */
 /* version: 1.0                                              */
-/* Description: this program determines whether the input 'n'*/
-/*              is a squareful number.                       */
+/* Description: this program counts the amount of mulpals   '*/
+/*              with a <= n <= b                             */
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
 
-    int lowBound, upBound;
+    // declare variables used for calculating the reverse n
+    int n,r,reverseN,temp;
+    int min,max;
+    int count = 0;
+    scanf("%d %d", &min, &max);
 
-    scanf("%d %d", &lowBound, &upBound);
+    for(n = min; n <= max; n++) {
 
+        // assign variables used for calculating the reverse n
+        temp = n;
+        reverseN = 0;
 
+        // algorithmically find reverse n
+        while (temp) {
+            r = temp % 10;
+            temp = temp / 10;
+            reverseN = reverseN * 10 + r;
+        }
 
+        // if there is no remainder, then the number n is a multipal
+        if(reverseN % n == 0){
+            //int d = reverseN / n;
+            //printf("%d * %d = %d\n", n, d, reverseN);
+            count++;
+        }
+
+    }
+
+    printf("%d",count);
     return 0;
 }
--- week2/src/triplets.c
+++ week2/src/triplets.c
@@ -17,31 +17,79 @@ int main(int argc, char *argv[]) {
     int count = 0;
     int n = input;
 
-    for (int a = 1; a < n; ++a) {
+    // below is used to print max value of n1 iteration.
+     int biggest_a = 0, biggest_b = 0, biggest_c = 0;
 
-        if(n % a == 0) {
+     int prevstep = 0;
 
-            int b = n / a;
+    // first we loop over all possible n1 values, 1 >= n1 < n
+    for (int n1 = 1260; n1 >= 1; --n1) {
 
-            printf("a = %d \n",a);
+//        if(n1 > n){
+//            continue;
+//        }
 
-            while (b > a){
+        // if we can divide n by n1 without leaving a remainder, continue
+        if(n % n1 == 0) {
 
-                int c = n / b / a;
 
-                if (n % b == 0) {
 
-                    if((a * b * c) == n){
+            int n2 = n / n1;
+
+            printf("(%d, %d)\n ",n1, n2);
+
+//            if(n2 < n1){
+//                break;
+//            }
+
+            while (n2 >= n1){
+
+                int c = n / n2 / n1;
+
+                // if n is divisible by n2 without leaving a remainder, continue.
+                if (n % n2 == 0) {
+                   // printf("(%d, %d, %d) ",n1, n2, c);
+
+                    // conditions to make sure we're not counting duplicates.
+                    if(n1 > c || (n1 == n2 && c != n1)  ||  (n1 < c && n1 < n2 && n2 > c)){
+//                        printf("-> skipping \n");
+                        n2--;
+                        continue;
+                    }
+
+                    if((n1 * n2 * c) == n){
+
+//                        int diff_a = n1 - biggest_a;
+//                        int diff_b = n2 - biggest_b;
+//                        int diff_c = c  - biggest_c;
+
+                        //printf("diff(%d, %d, %d)\n", diff_a, diff_b, diff_c);
+
+//                        if(n1 > biggest_a && n1 != 2000000000){
+//                            biggest_a = n1;
+//                        }
+//                        if(n2 > biggest_b && n2 != 2000000000){
+//                            biggest_b = n2;
+//                        }
+//                        if(c > biggest_c && c != 2000000000){
+//                            biggest_c = c;
+//                        }
                         count++;
-                        printf("(%d, %d, %d) \n",a, b, c);
+
+                        int steps = (n2 - n1);
+
+                        printf("%d -> counting (steps = %d, delta = %d)\n", count, steps, prevstep - steps);
+
+                        prevstep = steps;
                     }
                 }
-                b--;
+                n2--;
             }
 
         }
     }
 
+    printf("biggest (%d, %d, %d)\n", biggest_a, biggest_b, biggest_c);
     printf("%d", count);
 
     return 0;
